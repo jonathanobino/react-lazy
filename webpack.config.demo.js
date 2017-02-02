1a7fc288b5b6116f88237efdd088eccf7bb31cd5
Jonathan <jonobin@gmail.com>
2017-02-02 14:46:43 +0100

    fix indentation

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,26 +1,26 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-  entry:'./demo/demo.js',
-  output: {
-    path: path.join(__dirname),
-    filename: './demo/index.js'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-      __DEVELOPMENT__: false,
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-  ],
-  module: {
-    rules: [{
-     test: /\.js$/,
-     use: 'babel-loader',
-     exclude: /node_modules/
-   }]
-  }
-};
+	entry:'./demo/demo.js',
+	output: {
+		path: path.join(__dirname),
+		filename: './demo/index.js'
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"',
+			},
+			__DEVELOPMENT__: false,
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	],
+	module: {
+		rules: [{
+			test: /\.js$/,
+			use: 'babel-loader',
+			exclude: /node_modules/
+	 }]
+	}
+}
