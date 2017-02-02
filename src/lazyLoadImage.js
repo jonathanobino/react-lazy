3d8cc9a5843ff8bd8b9f6b0da33b97e152938c45
Jonathan <jonobin@gmail.com>
2017-02-02 14:14:11 +0100

    Started implementing new mechanism to avoid multiple window scroll listeners

--- src/lazyLoadImage.js
+++ src/lazyLoadImage.js
@@ -6,6 +6,12 @@ export default class LazyImage extends CheckIfRender {
 		super(props)
 	}
 	render(){
+		let style = this.props.style
+		if(this.state.link === '' && !style.height && !style.width){
+			style = Object.assign(style, {
+				height:'300px'
+			})
+		}
 		return <img src={this.state.link} alt={this.props.alt} style={this.props.style}></img>
 	}
 }
