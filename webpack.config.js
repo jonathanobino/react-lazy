83b0015d1a75516e9d41c757656d2a8890c7475e
Jonathan Obino <jonobin@gmail.com>
2017-05-03 12:38:37 +0200

    fixed prettier running on builded files

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,48 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-	entry:'./src/index.js',
-	devtool: 'source-map',
-	output: {
-		library: 'LazyReact',
-		libraryTarget: 'umd',
-		path: path.join(__dirname),
-		filename: './dist/index.js'
-	},
-	externals: [{
-		'react': {
-			root: 'React',
-			commonjs2: 'react',
-			commonjs: 'react',
-			amd: 'react'
-		}
-	}, {
-		'react-dom': {
-			root: 'ReactDOM',
-			commonjs2: 'react-dom',
-			commonjs: 'react-dom',
-			amd: 'react-dom'
-		}
-	}],
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"',
-			},
-			__DEVELOPMENT__: false,
-		}),
-		new webpack.optimize.UglifyJsPlugin({comments: false })
-	],
-	module: {
-		rules: [{
-			test: /\.js$/,
-			use: 'babel-loader',
-			exclude: /node_modules/
-		}]
-	}
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    library: 'LazyReact',
+    libraryTarget: 'umd',
+    path: path.join(__dirname),
+    filename: './dist/index.min.js'
+  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
+    }
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      },
+      __DEVELOPMENT__: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({ comments: false })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
