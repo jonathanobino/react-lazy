0eaf087a1e45e201b66446beb5b00ed2713d894f
Jonathan Obino <jonathanobino@gmail.com>
2022-04-11 14:55:50 +0200

    webpack format for codeclimate

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode:'production',
-  entry: path.join(__dirname,'./src/index.tsx'),
+  mode: 'production',
+  entry: path.join(__dirname, './src/index.tsx'),
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: [{loader:'ts-loader'}],
+        use: [{ loader: 'ts-loader' }],
         exclude: /node_modules/,
       },
     ],
@@ -19,7 +19,7 @@ module.exports = {
     library: 'LazyReact',
     libraryTarget: 'umd',
     path: path.join(__dirname),
-    filename: './dist/index.js'
+    filename: './dist/index.js',
   },
   externals: [
     {
@@ -27,16 +27,16 @@ module.exports = {
         root: 'React',
         commonjs2: 'react',
         commonjs: 'react',
-        amd: 'react'
-      }
+        amd: 'react',
+      },
     },
     {
       'react-dom': {
         root: 'ReactDOM',
         commonjs2: 'react-dom',
         commonjs: 'react-dom',
-        amd: 'react-dom'
-      }
-    }
+        amd: 'react-dom',
+      },
+    },
   ],
-};
+}
