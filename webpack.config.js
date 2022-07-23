2c6ee18adb459e1e528dcbac76cb862e1a94d6ed
Jonathan Obino <jonathanobino@gmail.com>
2022-07-23 12:43:43 +0200

    🐛 update externals

--- webpack.config.js
+++ webpack.config.js
@@ -21,22 +21,18 @@ module.exports = {
     path: path.join(__dirname),
     filename: './dist/index.js',
   },
-  externals: [
-    {
-      react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react',
-      },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
     },
-    {
-      'react-dom': {
-        root: 'ReactDOM',
-        commonjs2: 'react-dom',
-        commonjs: 'react-dom',
-        amd: 'react-dom',
-      },
-    },
-  ],
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    }
+  }
 }
